c3e356ada63f23e5838a837b2b3b8f6a2e75adb5
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2021-03-23 12:33:21 +0200

    Update tenshimoe-random-btn.js

--- tenshimoe-random-btn.js
+++ tenshimoe-random-btn.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tenshi.moe Random Anime
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  A userscript that adds a random button to Tenshi.moe
 // @author       Arjix
 // @match        *://*tenshi.moe/*
@@ -19,26 +19,21 @@ function getRandomInt(min, max) {
 
 (function() {
     'use strict';
-    const link = "https://raw.githubusercontent.com/AltairGreek/tenshi.moe-random/main/all_anime.json"
-    GM_xmlhttpRequest({
+    const link = "https://raw.githubusercontent.com/ArjixGamer/tenshimoe-random/main/all_anime.json"
+    const getRandomAnime = () => {
+        GM_xmlhttpRequest({
             method: "GET",
             url: link,
             onload: function(res) {
                 const ALL_ANIME = JSON.parse(res.response)
-                const getRandomAnime = () => {
-                    const index = getRandomInt(1, ALL_ANIME.length-1)
-                    document.location.href = ALL_ANIME[index]
-                }
-                const button = `<li id="menu-account" class="nav-item dropdown">
-                       <a class="nav-link btn btn-outline-secondary" id="randomAnimeBtn" href="#" role="button">
-                           <i class="fas fa-dice" aria-hidden="true"></i> <span class="d-none d-md-inline">Random</span>
-                       </a>
-                    </li>`
-                $("ul > li.nav-item.dropdown").first().before(button)
-                document.getElementById("randomAnimeBtn").onclick = getRandomAnime
-            }
-    })
+                const index = getRandomInt(1, ALL_ANIME.length-1)
+                document.location.href = ALL_ANIME[index]
+            }})}
+   const button = `<li id="menu-account" class="nav-item dropdown">
+                     <a class="nav-link btn btn-outline-secondary" id="randomAnimeBtn" href="#" role="button">
+                        <i class="fas fa-dice" aria-hidden="true"></i> <span class="d-none d-md-inline">Random</span>
+                     </a>
+                  </li>`
+   $("ul > li.nav-item.dropdown").first().before(button)
+   document.getElementById("randomAnimeBtn").onclick = getRandomAnime
 })();
-
-
-
